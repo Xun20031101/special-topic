76667b9acbf44f2feec3450d41b41b7a99b340dd
Xun20031101 <64963703+Xun20031101@users.noreply.github.com>
2021-05-10 16:58:27 +0800

    Update rfid.h

--- smartphonelock_new/special-topic/special-topic/smartphoneLock/rfid.h
+++ smartphonelock_new/special-topic/special-topic/smartphoneLock/rfid.h
@@ -1,16 +1,18 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN      8  // 讀卡機的重置腳位
-#define SS_PIN       9  // 晶片選擇腳位
+#define RST_PIN      A0  // 讀卡機的重置腳位
+#define SS_PIN       10  // 晶片選擇腳位
 
+char tag[5]="open";
+ 
 struct RFIDTag {   // 定義結構
    byte uid[4];
    char *name;
 };
 
 struct RFIDTag tags[] = {  // 初始化結構資料，請自行修改RFID識別碼。
-  {{99,31,111,12},"1"}
+  {{99,31,111,12},tag}
 };
 
 byte totalTags = sizeof(tags) / sizeof(RFIDTag);  // 計算結構資料筆數，結果為3。
