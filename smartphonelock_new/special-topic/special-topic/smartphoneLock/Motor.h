ad97ce31f4357cc76fb8f1ee29923006490d7578
Xun20031101 <64963703+Xun20031101@users.noreply.github.com>
2021-05-17 16:48:04 +0800

    Update Motor.h

--- smartphonelock_new/special-topic/special-topic/smartphoneLock/Motor.h
+++ smartphonelock_new/special-topic/special-topic/smartphoneLock/Motor.h
@@ -1,13 +1,13 @@
 #include <Servo.h>
-#define SERVO_PIN    2   // 伺服馬達的控制訊號接腳
+#define SERVO_PIN    9  // 伺服馬達的控制訊號接腳
 
-Servo servo; // 建立Servo物件，控制伺服馬達
+Servo servo;
 bool lockerSwitch = false;  // 伺服馬達的狀態
 
 // 開鎖或關鎖
 void locker(bool toggle)
 {
-  servo.attach(SERVO_PIN);  // 將伺服馬達物件附加在數位2腳
+  servo.attach(SERVO_PIN);  // 
   if (toggle)
   {
       servo.write(90);  // 開鎖
